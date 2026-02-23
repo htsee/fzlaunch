f517c460dff22cf36b82d20698933bf3cab0eccb
Teddy See <teddy@tsee.dev>
2026-02-23 18:37:39 +0000

    feat: add run command

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/htsee/fzlaunch/internal"
 	"github.com/spf13/cobra"
@@ -32,6 +33,31 @@ var ListCmd = &cobra.Command{
 	},
 }
 
+var RunCmd = &cobra.Command{
+	Use:   "run",
+	Short: "run application",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		appName := args[0]
+		entries, err := internal.DesktopEntries()
+		if err != nil {
+			return err
+		}
+		for _, entry := range entries {
+			if appName == entry.Name {
+				cmd := exec.Command(entry.Exec)
+				if err := cmd.Run(); err != nil {
+					return err
+				}
+				return nil
+			}
+		}
+		fmt.Printf("cannot find application %v", appName)
+		return nil
+	},
+}
+
 func init() {
 	RootCmd.AddCommand(ListCmd)
+	RootCmd.AddCommand(RunCmd)
 }
