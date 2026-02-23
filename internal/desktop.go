da474e820499e8d5598630d57509394aaad68ae3
Teddy See <teddy@tsee.dev>
2026-02-23 17:08:51 +0000

    feat: find desktop files

--- internal/desktop.go
+++ internal/desktop.go
@@ -2,9 +2,29 @@ package internal
 
 import (
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 var xdgDataDirs = strings.Split(os.Getenv("XDG_DATA_DIRS"), ":")
 var xdgDataHome = strings.Split(os.Getenv("XDG_DATA_HOME"), ":")
 var XdgData = append(xdgDataDirs, xdgDataHome...)
+
+func GetDesktop() []string {
+	var desktop []string
+	for _, dir := range XdgData {
+		appDir := dir + "/applications"
+		files, err := os.ReadDir(appDir)
+		if err != nil {
+			continue
+		}
+
+		for _, file := range files {
+			fileName := file.Name()
+			if filepath.Ext(fileName) == ".desktop" {
+				desktop = append(desktop, fileName)
+			}
+		}
+	}
+	return desktop
+}
--- main.go
+++ main.go
@@ -7,5 +7,5 @@ import (
 )
 
 func main() {
-	fmt.Println(internal.XdgData)
+	fmt.Println(internal.GetDesktop())
 }
