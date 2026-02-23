3960d26c7c13c576fcc63e2af0656723743566c3
Teddy See <teddy@tsee.dev>
2026-02-23 19:48:14 +0000

    feat: nix build

--- flake.nix
+++ flake.nix
@@ -16,6 +16,14 @@
       };
     in
     {
+      packages.${system}.default = pkgs.buildGoModule {
+        name = "fzlaunch";
+        version = "0.1.0";
+        src = self;
+        goSum = ./go.sum;
+        vendorHash = "sha256-7K17JaXFsjf163g5PXCb5ng2gYdotnZ2IDKk8KFjNj0=";
+      };
+
       devShells."x86_64-linux".default = pkgs.mkShell {
         packages = with pkgs; [
           go
